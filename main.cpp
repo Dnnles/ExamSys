c3c1c5e44376f594487f4f8aed2805d450d03802
xianzhicc <xianzhi1012@163.com>
2021-04-01 23:22:15 +0800

    此项目为qt入门项目,账号密码在account文件夹下

---
+++ main.cpp
@@ -0,0 +1,15 @@
+﻿#include "logindialog.h"
+#include "examdialog.h"
+
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    //loginDialog w;
+    //w.show();
+    examdialog e;
+    e.show();
+
+    return a.exec();
+}
